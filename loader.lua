9f8727ca95f22db9e24bc02f28f1efea2f48e0aa
path <patheticcs@gmail.com>
2025-03-13 15:45:09 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,27 @@
+--[[
+
+░░░░░██╗░█████╗░██╗███╗░░██╗  ███╗░░░███╗██╗░░░██╗  ██████╗░██╗░██████╗░█████╗░░█████╗░██████╗░██████╗░
+░░░░░██║██╔══██╗██║████╗░██║  ████╗░████║╚██╗░██╔╝  ██╔══██╗██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+░░░░░██║██║░░██║██║██╔██╗██║  ██╔████╔██║░╚████╔╝░  ██║░░██║██║╚█████╗░██║░░╚═╝██║░░██║██████╔╝██║░░██║
+██╗░░██║██║░░██║██║██║╚████║  ██║╚██╔╝██║░░╚██╔╝░░  ██║░░██║██║░╚═══██╗██║░░██╗██║░░██║██╔══██╗██║░░██║
+╚█████╔╝╚█████╔╝██║██║░╚███║  ██║░╚═╝░██║░░░██║░░░  ██████╔╝██║██████╔╝╚█████╔╝╚█████╔╝██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚═╝╚═╝░░╚══╝  ╚═╝░░░░░╚═╝░░░╚═╝░░░  ╚═════╝░╚═╝╚═════╝░░╚════╝░░╚════╝░╚═╝░░╚═╝╚═════╝░
+
+https://discord.gg/uGxSYkyp66
+
+--]]
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/rivalsscriptapi/refs/heads/main/testing.lua?token=GHSAT0AAAAAADAG6J6D5DETRLYODOVITE4UZ6TMARQ",true))()
+
+local Config = {
+    SENSITIVITY_MULTIPLIER = 0.5,
+    AimbotMode = "Hold",
+    ESPEnabled = false,
+    AutoShootEnabled = false,
+    InfiniteJumpEnabled = false,
+    NoClipEnabled = false,
+    WalkSpeedEnabled = false,
+    WalkSpeed = 50,
+    AimbotKeybind = Enum.UserInputType.MouseButton2
+}
