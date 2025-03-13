2fd7657cf233d382e9203c994d7ae837f9fe5c1c
path <patheticcs@gmail.com>
2025-03-13 15:59:52 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -13,17 +13,17 @@ https://discord.gg/uGxSYkyp66
 
 
 local Config = {
-    SENSITIVITY_MULTIPLIER = 100,
+    SENSITIVITY_MULTIPLIER = 5,
     AimbotMode = "Hold",
-    ESPEnabled = true,
-    AutoShootEnabled = true,
-    InfiniteJumpEnabled = true,
-    NoClipEnabled = true,
-    WalkSpeedEnabled = true,
+    ESPEnabled = false,
+    AutoShootEnabled = false,
+    InfiniteJumpEnabled = false,
+    NoClipEnabled = false,
+    WalkSpeedEnabled = false,
     WalkSpeed = 100,
     AimbotKeybind = Enum.UserInputType.MouseButton2
 }
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/rivalsscriptapi/refs/heads/main/testing.lua", true))()
-
 getfenv(1).Config = Config
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/rivalsscriptapi/refs/heads/main/testing.lua", true))()
