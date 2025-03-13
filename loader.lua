713e0d038c8e12be7d4fe92353decbe8c448f239
path <patheticcs@gmail.com>
2025-03-13 15:47:43 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,7 +12,7 @@ https://discord.gg/uGxSYkyp66
 --]]
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/rivalsscriptapi/refs/heads/main/testing.lua?token=GHSAT0AAAAAADAG6J6D5DETRLYODOVITE4UZ6TMARQ",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/rivalsscriptapi/refs/heads/main/testing.lua",true))()
 
 local Config = {
     SENSITIVITY_MULTIPLIER = 0.5,
